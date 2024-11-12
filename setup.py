c9aa25053a844a0167ca04d9011fba31d4a8ca1a
maks-137 <111008484+maks-137@users.noreply.github.com>
2024-11-12 16:12:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,5 @@
 from setuptools import setup, find_packages
 
-requires=[
-	'tensorflow==2.10.0',
-	'Pillow'
-    ]
-
-
-
 setup(
     name='gender_classifier',
     version='1.0.0',
@@ -14,7 +7,6 @@ setup(
     author_email='maks.remezovskij1@gmail.com',
     url='https://github.com/maksr137/gender_classifier',
     packages=find_packages(),
-    install_requires=requires,
     entry_points = {
        'console_scripts': [
            'gender-classifier=api.main:predict'
